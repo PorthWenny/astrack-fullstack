d4d2cfd2147082c9f2c2534ae5afccb231ffbb08
Bayla Angelo <111470903+PorthWenny@users.noreply.github.com>
2024-11-13 09:07:03 +0800

    🚑 Fix facilities controller.

--- api/controllers/facilities.controller.js
+++ api/controllers/facilities.controller.js
@@ -1,79 +1,71 @@
-import "prisma" from "../lib/prisma.js";
+import prisma from "../lib/prisma.js";
 
-export const getFacilities = async (req,res) => {
-    try {
+export const getFacilities = async (req, res) => {
+  try {
+    const facilities = await prisma.facilities.findMany();
 
-        const facilities = await prisma.facilities.findMany()
+    res.status(200).json(facilities);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to get records" });
+  }
+};
 
-        res.status(200).json(facilities)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message : "Failed to get records"})
-    }
-}
+export const getFacility = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const facility = await prisma.facilities.findUnique({
+      where: { id },
+    });
 
+    res.status(200).json(facility);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to get records" });
+  }
+};
 
-export const getFacility = async (req,res) => {
-    const id = req.params.id;
-    try {
+export const addFacility = async (req, res) => {
+  const body = req.body;
 
-        const facility = await prisma.facilities.findUnique({
-            where:{id},
-        })
+  try {
+    const newFacility = await prisma.facilities.create({
+      data: {
+        ...body,
+      },
+    });
 
-        res.status(200).json(facility)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message : "Failed to get records"})
-    }
-}
+    res.status(200).json(newFacility);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to get records" });
+  }
+};
 
+export const updFacility = async (req, res) => {
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to get records" });
+  }
+};
 
-export const addFacility = async (req,res) => {
-    const body = req.body;
+export const delFacility = async (req, res) => {
+  const id = req.params.id;
 
-    try {
+  try {
+    const facility = await prisma.facilities.findUnique({
+      where: { id },
+    });
 
-        const newFacility = await prisma.facilities.create({
-            data: {
-                ...body,
-            }
-        })
+    await prisma.facilities.delete({
+      where: { id },
+    });
 
-        res.status(200).json(newFacility)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message : "Failed to get records"})
-    }
-}
-
-
-export const updFacility = async (req,res) => {
-    try {
-        res.status(200).json()
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message : "Failed to get records"})
-    }
-}
-
-
-export const delFacility = async (req,res) => {
-    const id = req.params.id;
-
-    try {
-
-        const post = await prisma.facilities.findUnique({
-            where: { id },
-        })
-
-        await prisma.post.delete({
-            where: { id },
-        })
-       
-        res.status(200).json(message: "Successfuly deleted record   ")
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message : "Failed to get records"})
-    }
-}
+    res.status(200).json({ message: "Successfuly deleted record" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to get records" });
+  }
+};
